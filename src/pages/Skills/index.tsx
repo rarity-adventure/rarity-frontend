ab42c454a6c28bee62de9b3d7ea9d7514fc8d15c
eabz <eabz@polispay.org>
2021-09-10 10:23:52 -0500

    WIP remove

--- src/pages/Skills/index.tsx
+++ src/pages/Skills/index.tsx
@@ -1,8 +1,8 @@
 import { useUserSummoners } from '../../state/user/hooks'
 import { Summoner } from '../../state/user/actions'
 import { CLASSES } from '../../constants/classes'
-import stats from '../../assets/images/stats.png'
-import title from '../../assets/images/stats_txt.png'
+import skills from '../../assets/images/skills.png'
+import skills_txt from '../../assets/images/skills_text.png'
 import { useState } from 'react'
 import SummonerSkillsCard from '../../components/Summoner/Skills'
 
@@ -18,8 +18,8 @@ export default function Skills(): JSX.Element | null {
     return (
         <>
             <div className="w-full mb-44">
-                <img alt="sword" src={stats} className="mx-auto w-16 mt-4 md:w-32" />
-                <img alt="sword" src={title} className="mx-auto w-52 mt-4 md:w-64" />
+                <img alt="sword" src={skills} className="mx-auto w-16 mt-4 md:w-32" />
+                <img alt="sword" src={skills_txt} className="mx-auto w-52 mt-4 md:w-64" />
             </div>
             <h1 className="text-md md:text-2xl text-white -mt-32 mb-12 uppercase">Assign skills to your summoners</h1>
             <div className="w-full bg-custom-blue text-center pb-24">
