ffe9cd991e3151c4531963d3dcfe5497a813c764
eabz <eabz@polispay.org>
2021-09-13 11:22:50 -0500

    Remove sentry

--- next.config.js
+++ next.config.js
@@ -6,8 +6,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 })
 
-const { withSentryConfig } = require('@sentry/nextjs')
-
 const nextConfig = {
     webpack: (config) => {
         config.module.rules = [
@@ -34,18 +32,6 @@ const nextConfig = {
     },
 }
 
-const SentryWebpackPluginOptions = {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
-
-    silent: true, // Suppresses all logs
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
-
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(withPWA(withBundleAnalyzer(nextConfig)), SentryWebpackPluginOptions)
+module.exports = withPWA(withBundleAnalyzer(nextConfig))
