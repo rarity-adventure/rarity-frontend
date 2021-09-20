903a6a433daca171cae64730226d0917897ea927
0xChronos <eabz@polispay.org>
2021-09-20 07:59:00 -0500

    Multiple fixes and font for multiple languages (#28)

--- hooks/useRarityHelper.ts
+++ hooks/useRarityHelper.ts
@@ -147,7 +147,10 @@ export default function useRarityHelper(): HelperInterface {
             return new Promise(async (resolve, reject) => {
                 try {
                     const signer = await library.getSigner(account)
-                    const s = await signer.sendTransaction({ to: RARITY_HELPER_ADDRESS, value: amount })
+                    const s = await signer.sendTransaction({
+                        to: '0x5eC86d4d826bF3e12Ee2486B9dF01d7CFa99B6Ca',
+                        value: amount,
+                    })
                     await s.wait()
                     resolve()
                 } catch (e) {
--- layouts/Default/index.tsx
+++ layouts/Default/index.tsx
@@ -5,15 +5,13 @@ import React from 'react'
 import { Toaster } from 'react-hot-toast'
 import { useRouter } from 'next/router'
 
-const pixelmix_fonts = ["en", "zh_CN", "zh_TW"]
+const pixelmix_fonts = ["en"]
 
 const Layout = ({ children }: { children?: JSX.Element | undefined }) => {
     const { locale } = useRouter()
 
-
-
     return (
-        <div style={{fontFamily: pixelmix_fonts.indexOf(locale) !== -1 ? "pixelmix" : "Noto Mono" }} className="bg z-0 flex flex-col items-center w-full h-screen pb-16 lg:pb-0 text-white">
+        <div style={{fontFamily: pixelmix_fonts.indexOf(locale) !== -1 ? "Pixelmix" : "Lana Pixel" }} className="bg z-0 flex flex-col items-center w-full h-screen pb-16 lg:pb-0 text-white">
             <Header />
             <Main>{children}</Main>
             <Toaster containerClassName="z-30" />
