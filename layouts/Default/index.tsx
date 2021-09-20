b66face2cd63413d679adb14870a3a4bf6515204
eabz <eabz@polispay.org>
2021-09-20 07:58:23 -0500

    Keep pixelmix for english

--- layouts/Default/index.tsx
+++ layouts/Default/index.tsx
@@ -3,10 +3,15 @@ import Header from '../../components/Header'
 import Main from '../../components/Main'
 import React from 'react'
 import { Toaster } from 'react-hot-toast'
+import { useRouter } from 'next/router'
+
+const pixelmix_fonts = ["en"]
 
 const Layout = ({ children }: { children?: JSX.Element | undefined }) => {
+    const { locale } = useRouter()
+
     return (
-        <div className="bg z-0 flex flex-col items-center w-full h-screen pb-16 lg:pb-0 text-white">
+        <div style={{fontFamily: pixelmix_fonts.indexOf(locale) !== -1 ? "Pixelmix" : "Lana Pixel" }} className="bg z-0 flex flex-col items-center w-full h-screen pb-16 lg:pb-0 text-white">
             <Header />
             <Main>{children}</Main>
             <Toaster containerClassName="z-30" />
