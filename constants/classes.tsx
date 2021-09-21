b97dc1e8384d052d2f7871551da00b78d00089a9
eabz <eabz@polispay.org>
2021-09-21 09:37:51 -0500

    Reduce sizes

--- constants/classes.tsx
+++ constants/classes.tsx
@@ -16,17 +16,17 @@ export const CLASSES_NAMES: { [k: string]: string } = {
     '11': 'wizard',
 }
 export const CLASSES_IMAGES: { [k: string]: JSX.Element } = {
-    '1': <Image src="/img/classes/barbarian.png" width={103} height={210} alt={'barbarian'} />,
-    '2': <Image src="/img/classes/bard.png" width={100} height={210} alt={'bard'} />,
-    '3': <Image src="/img/classes/cleric.png" width={135} height={210} alt={'cleric'} />,
-    '4': <Image src="/img/classes/druid.png" width={80} height={210} alt={'druid'} />,
-    '5': <Image src="/img/classes/fighter.png" width={80} height={210} alt={'fighter'} />,
-    '6': <Image src="/img/classes/monk.png" width={105} height={210} alt={'monk'} />,
-    '7': <Image src="/img/classes/paladin.png" width={90} height={210} alt={'paladin'} />,
-    '8': <Image src="/img/classes/ranger.png" width={107} height={210} alt={'ranger'} />,
-    '9': <Image src="/img/classes/rogue.png" width={110} height={210} alt={'rogue'} />,
-    '10': <Image src="/img/classes/sorcerer.png" width={115} height={210} alt={'sorcerer'} />,
-    '11': <Image src="/img/classes/wizard.png" width={90} height={210} alt={'wizard'} />,
+    '1': <Image src="/img/classes/barbarian.png" width={85} height={175} alt={'barbarian'} />,
+    '2': <Image src="/img/classes/bard.png" width={80} height={175} alt={'bard'} />,
+    '3': <Image src="/img/classes/cleric.png" width={115} height={175} alt={'cleric'} />,
+    '4': <Image src="/img/classes/druid.png" width={70} height={175} alt={'druid'} />,
+    '5': <Image src="/img/classes/fighter.png" width={65} height={175} alt={'fighter'} />,
+    '6': <Image src="/img/classes/monk.png" width={85} height={175} alt={'monk'} />,
+    '7': <Image src="/img/classes/paladin.png" width={75} height={175} alt={'paladin'} />,
+    '8': <Image src="/img/classes/ranger.png" width={87} height={175} alt={'ranger'} />,
+    '9': <Image src="/img/classes/rogue.png" width={90} height={175} alt={'rogue'} />,
+    '10': <Image src="/img/classes/sorcerer.png" width={100} height={175} alt={'sorcerer'} />,
+    '11': <Image src="/img/classes/wizard.png" width={75} height={175} alt={'wizard'} />,
 }
 
 export const CLASS_SKILLS: { [k: string]: boolean[] } = {
