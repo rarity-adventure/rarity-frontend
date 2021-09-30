c13d4e627fe005e7965749484db070822b94f8ba
Tasio Victoria <tasiovictoria@gmail.com>
2021-09-29 22:43:53 -0400

    Check for adventure approvals on the Rarity contract before allowing daycare registration in DaycareMulti modal

--- components/Modal/modals/DaycareMulti.tsx
+++ components/Modal/modals/DaycareMulti.tsx
@@ -1,11 +1,14 @@
 import HeadlessUIModal from '../HeadlessUIModal'
 import ModalHeader from '../ModalHeader'
 import { t } from '@lingui/macro'
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { useLingui } from '@lingui/react'
 import toast from 'react-hot-toast'
+import { RARITY_HELPER_ADDRESS } from '../../../constants'
 import { SummonerFullData } from '../../../hooks/useRarityLibrary'
 import useRarityDaycare from '../../../hooks/useRarityDaycare'
+import useRarity from '../../../hooks/useRarity'
+import useActiveWeb3React from '../../../hooks/useActiveWeb3React'
 
 interface TransferModalProps {
     open: boolean
@@ -16,9 +19,34 @@ interface TransferModalProps {
 export default function DaycareMultiModal({ open, closeFunction, summoners }: TransferModalProps): JSX.Element {
     const { i18n } = useLingui()
 
+    const { account } = useActiveWeb3React()
+
     const { registerDaycare } = useRarityDaycare()
 
+    const { isApprovedForAll, setApprovalForAll } = useRarity()
+
     const [days, setDays] = useState(0)
+
+    const [approved, setApproved] = useState(false)
+
+    const fetch_approval = useCallback(async () => {
+        const approved = await isApprovedForAll(account, RARITY_HELPER_ADDRESS)
+        setApproved(approved)
+    }, [account, isApprovedForAll])
+
+    useEffect(() => {
+        fetch_approval()
+    }, [summoners, fetch_approval])
+
+    async function approveHelper() {
+        toast
+            .promise(setApprovalForAll(RARITY_HELPER_ADDRESS), {
+                loading: <b>{i18n._(t`Approving helper contract`)}</b>,
+                success: <b>{i18n._(t`Success`)}</b>,
+                error: <b>{i18n._(t`Failed`)}</b>,
+            })
+            .then(() => setApproved(true))
+    }
 
     async function registerConfirm() {
         await toast.promise(
@@ -50,20 +78,36 @@ export default function DaycareMultiModal({ open, closeFunction, summoners }: Tr
                 <div className="text-center text-white p-4 pb-2 gap-5">
                     <h2>{i18n._(t`How many days do you want to register your summoners?`)}</h2>
                 </div>
-                <div className="text-center text-white p-4 pb-4 gap-5">
-                    <input
-                        type="number"
-                        className="p-2 text-background-end"
-                        onChange={(v) => setDays(parseInt(v.target.value))}
-                    />
-                </div>
-                <div className="flex flex-row justify-center pb-8">
-                    <div className="bg-background-middle hover:bg-background-start text-white border-white border-2 rounded-lg mx-4">
-                        <button className="w-full uppercase px-2 py-1" onClick={async () => await registerConfirm()}>
-                            <h2>{i18n._(t`register summoners`)}</h2>
+                {approved ? (
+                    <>
+                        <div className="text-center text-white p-4 pb-4 gap-5">
+                            <input
+                                type="number"
+                                className="p-2 text-background-end"
+                                onChange={(v) => setDays(parseInt(v.target.value))}
+                            />
+                        </div>
+                        <div className="flex flex-row justify-center pb-8">
+                            <div className="bg-background-middle hover:bg-background-start text-white border-white border-2 rounded-lg mx-4">
+                                <button
+                                    className="w-full uppercase px-2 py-1"
+                                    onClick={async () => await registerConfirm()}
+                                >
+                                    <h2>{i18n._(t`register summoners`)}</h2>
+                                </button>
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <div className="text-center text-white p-4 pb-16 gap-5">
+                        <button
+                            onClick={() => approveHelper()}
+                            className="bg-green border-white border-2 p-2 uppercase rounded-lg mt-4"
+                        >
+                            {i18n._(t`approve helper`)}
                         </button>
                     </div>
-                </div>
+                )}
             </div>
         </HeadlessUIModal>
     )
