1da75394aec15d72943fccd333ce1c2f0b441f1b
Tasio Victoria <tasiovictoria@gmail.com>
2021-10-01 15:54:04 -0400

    Fix that allows for multiple skill assignments

--- components/Cards/Skills.tsx
+++ components/Cards/Skills.tsx
@@ -29,9 +29,10 @@ function SummonerSkillsCard({ summoner }: { summoner: SummonerFullData }): JSX.E
 
     function handleAddition(id: string) {
         const addition = additions[id] ? additions[id] + 1 : 1
+        const nextValue = summoner.skills.skills[parseInt(id) - 1] + addition
         const newState = Object.assign({}, additions, { [id]: addition })
         const sp = calcAvailableSP(newState)
-        if (addition <= maxLevel(id) && sp >= 0) {
+        if (nextValue <= maxLevel(id) && sp >= 0) {
             setAdditions(newState)
             setAvailableSP(sp)
         }
@@ -73,12 +74,11 @@ function SummonerSkillsCard({ summoner }: { summoner: SummonerFullData }): JSX.E
     const { set_skills } = useRaritySkills()
 
     async function assignSkills() {
-        const skills = new Array(36)
-        skills.fill(0)
+        const nextSkills = [...summoner.skills.skills]
         Object.keys(additions).map((k) => {
-            skills[parseInt(k) - 1] = additions[k]
+            nextSkills[parseInt(k) - 1] += additions[k]
         })
-        await toast.promise(set_skills(summoner.id, skills), {
+        await toast.promise(set_skills(summoner.id, nextSkills), {
             loading: <b>{i18n._(t`Assigning skill`)}</b>,
             success: <b>{i18n._(t`Success`)}</b>,
             error: <b>{i18n._(t`Failed`)}</b>,
