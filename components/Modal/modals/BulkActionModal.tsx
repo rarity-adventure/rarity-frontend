2e6fe62364184fd6c02eb1634367819752a66294
eabz <eabz@polispay.org>
2021-10-02 09:58:27 -0500

    Fix

--- components/Modal/modals/BulkActionModal.tsx
+++ components/Modal/modals/BulkActionModal.tsx
@@ -13,7 +13,7 @@ import { sendToast } from '../../../functions/toast'
 import { calcXPForNextLevel } from '../../../functions/calcXPForNextLevel'
 
 interface BulkActionModalProps {
-    a: BulkAction
+    action: BulkAction
     open: boolean
     closeFunction: () => void
     summoners: SummonerFullData[]
@@ -26,7 +26,7 @@ export enum BulkAction {
     DUNGEON,
 }
 
-export default function BulkActionModal({ open, closeFunction, summoners, a }: BulkActionModalProps): JSX.Element {
+export default function BulkActionModal({ open, closeFunction, summoners, action }: BulkActionModalProps): JSX.Element {
     const { i18n } = useLingui()
 
     const { account } = useActiveWeb3React()
@@ -54,12 +54,20 @@ export default function BulkActionModal({ open, closeFunction, summoners, a }: B
         fetch_approval()
     }, [summoners, fetch_approval])
 
-    async function submit(tip: boolean) {
-        const chunks = chunkArrayByNumber(filter_summoners(summoners), 200)
+    async function submit(tip: boolean, action: BulkAction) {
+        let chunks
+
+        if (action === BulkAction.LEVELUP || action === BulkAction.ADVENTURE) {
+            chunks = chunkArrayByNumber(summoners, 200)
+        } else {
+            chunks = chunkArrayByNumber(summoners, 100)
+        }
+
         for (let i = 0; i < chunks.length; i++) {
             if (tip && i === 0) {
+                const func = getBulkActionDonate(action)
                 await sendToast(
-                    bulk_action_donate(
+                    func(
                         chunks[i].map((s) => {
                             return s.id
                         })
@@ -67,8 +75,9 @@ export default function BulkActionModal({ open, closeFunction, summoners, a }: B
                     i18n._(t`Sending chunk:`) + ' ' + (i + 1).toString() + ' of ' + chunks.length
                 )
             } else {
+                const func = getBulkAction(action)
                 await sendToast(
-                    bulk_action(
+                    func(
                         chunks[i].map((s) => {
                             return s.id
                         })
@@ -79,29 +88,20 @@ export default function BulkActionModal({ open, closeFunction, summoners, a }: B
         }
     }
 
-    const [action, setAction] = useState(0)
-
-    useEffect(() => {
-        setAction(a)
-    }, [a])
-
-    const filter_summoners = useCallback(
-        (summoners: SummonerFullData[]): SummonerFullData[] => {
-            switch (action) {
-                case BulkAction.ADVENTURE:
-                    return summoners.filter((s) => s.base._log * 1000 < Date.now())
-                case BulkAction.LEVELUP:
-                    return summoners.filter((s) => s.base._xp >= calcXPForNextLevel(s.base._level))
-                case BulkAction.DUNGEON:
-                    return summoners.filter((s) => s.materials.log * 1000 < Date.now() && s.materials.scout !== 0)
-                case BulkAction.GOLD:
-                    return summoners.filter((s) => s.gold.claimable > 0)
-                default:
-                    return []
-            }
-        },
-        [action]
-    )
+    const filter_summoners = useCallback((summoners: SummonerFullData[], action: BulkAction): SummonerFullData[] => {
+        switch (action) {
+            case BulkAction.ADVENTURE:
+                return summoners.filter((s) => s.base._log * 1000 < Date.now())
+            case BulkAction.LEVELUP:
+                return summoners.filter((s) => s.base._xp >= calcXPForNextLevel(s.base._level))
+            case BulkAction.DUNGEON:
+                return summoners.filter((s) => s.materials.log * 1000 < Date.now() && s.materials.scout !== 0)
+            case BulkAction.GOLD:
+                return summoners.filter((s) => s.gold.claimable > 0)
+            default:
+                return []
+        }
+    }, [])
 
     const title = useMemo(() => {
         switch (action) {
@@ -116,7 +116,7 @@ export default function BulkActionModal({ open, closeFunction, summoners, a }: B
         }
     }, [action])
 
-    const bulk_action = useMemo(() => {
+    function getBulkAction(action: BulkAction) {
         switch (action) {
             case BulkAction.ADVENTURE:
                 return adventure
@@ -127,9 +127,9 @@ export default function BulkActionModal({ open, closeFunction, summoners, a }: B
             case BulkAction.GOLD:
                 return claim_gold
         }
-    }, [action, adventure, cellar, claim_gold, level_up])
+    }
 
-    const bulk_action_donate = useMemo(() => {
+    function getBulkActionDonate(action: BulkAction) {
         switch (action) {
             case BulkAction.ADVENTURE:
                 return adventure_donate
@@ -140,37 +140,44 @@ export default function BulkActionModal({ open, closeFunction, summoners, a }: B
             case BulkAction.GOLD:
                 return claim_gold_donate
         }
-    }, [action, adventure_donate, level_up_donate, cellar_donate, claim_gold_donate])
+    }
 
     return (
         <Modal isOpen={open} onDismiss={closeFunction}>
             <div className="bg-background-end rounded-lg border-2 border-white">
                 {title && <ModalHeader title={i18n._(title)} onClose={closeFunction} />}
                 <div className="text-center text-white p-4 pb-8 gap-5">
-                    {filter_summoners(summoners).length > 0 ? (
+                    {filter_summoners(summoners, action).length > 0 ? (
                         <div>
                             <h2>
-                                {i18n._(t`You have`)} {filter_summoners(summoners).length}{' '}
-                                {i18n._(t`summoners available to send for adventure.`)}{' '}
+                                {i18n._(t`You have`)} {filter_summoners(summoners, action).length}{' '}
+                                {i18n._(t`summoners available.`)}{' '}
                             </h2>
-                            {summoners.length >= 200 && (
-                                <h2 className="mt-1">{i18n._(t`We will send 1 transaction for each 200 summoners`)}</h2>
-                            )}
+                            {(action === BulkAction.ADVENTURE && summoners.length >= 200) ||
+                                (action === BulkAction.LEVELUP && summoners.length >= 200 && (
+                                    <h2 className="mt-1">
+                                        {i18n._(t`We will send 1 transaction for each 200 summoners`)}
+                                    </h2>
+                                ))}
+                            {(action === BulkAction.DUNGEON && summoners.length >= 100) ||
+                                (action === BulkAction.GOLD && summoners.length >= 100 && (
+                                    <h2 className="mt-1">
+                                        {i18n._(t`We will send 1 transaction for each 100 summoners`)}
+                                    </h2>
+                                ))}
                             {approved ? (
                                 <>
-                                    {filter_summoners(summoners).length >= 10 && (
-                                        <div>
-                                            <button
-                                                onClick={() => submit(true)}
-                                                className="bg-green border-white border-2 p-2 uppercase rounded-lg mt-4"
-                                            >
-                                                {i18n._(t`send with 0.1 FTM tip for devs`)}
-                                            </button>
-                                        </div>
-                                    )}
                                     <div>
                                         <button
-                                            onClick={() => submit(false)}
+                                            onClick={() => submit(true, action)}
+                                            className="bg-green border-white border-2 p-2 uppercase rounded-lg mt-4"
+                                        >
+                                            {i18n._(t`send with 0.1 FTM tip for devs`)}
+                                        </button>
+                                    </div>
+                                    <div>
+                                        <button
+                                            onClick={() => submit(false, action)}
                                             className="bg-green border-white border-2 p-2 uppercase rounded-lg mt-4"
                                         >
                                             {i18n._(t`send summoners`)}
